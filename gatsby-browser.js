f9ffc63952ec4973018d595976751bde1e8d5b6c
Sharon <sharon.peishan.kennedy@gmail.com>
2018-07-17 10:22:33 -0400

    add polyfill back in

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,23 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+import "babel-polyfill";
+import React from "react";
+import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import React from 'react'
-import { Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-
-
-import createStore from './src/redux/createStore'
+import createStore from "./src/redux/createStore";
 
 export const replaceRouterComponent = ({ history }) => {
-  const store = createStore()
+  const store = createStore();
 
   const ConnectedRouterWrapper = ({ children }) => (
     <Provider store={store}>
       <Router history={history}>{children}</Router>
     </Provider>
-  )
+  );
 
-  return ConnectedRouterWrapper
-}
+  return ConnectedRouterWrapper;
+};
